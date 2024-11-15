48a66f095e69f937899abcbbd892e19c80c89516
magovia <minorgom@hotmail.com>
2024-11-15 07:10:16 -0600

    Distleimi (#3)
    * Modify respaldo
    
    * Base de datos respaldada exitosamente
    
    * MOD: Modified code
    
    * MOD: fixed issue with frmOrdenesCompras
    
    * MOD: added button to save records in Frm_ActualizarPythonExe
    
    * NEW: added parrot icon
    
    * Adding executable file
    
    * MOD: Removed inv.accde
    
    * MOD: added msgbox
    
    * MOD: check points to verify issues while loading the invoices
    
    * NEW: checkpoints added
    
    ---------
    
    Co-authored-by: laRedCr <ventas@laredcr.com>

--- respaldo.bat
+++ respaldo.bat
@@ -4,18 +4,18 @@
 echo ===========================
 echo    Sistema de respaldo
 echo 	v1.0
-echo	git branch XxxxXX
+echo	git branch distleimi
 echo ===========================
 
 echo ATENCION: Asegurese de tener conexion a internet...
 Pause
 
-cd /d "%~dp0"           :: Changes directory to the location of the batch file
+git add inv_backEnd.accdb 
+git commit -m "Base de datos respaldada exitosamente"
+git push
 
-git add mydata.csv      :: Stage the specific file you want to push
-git commit -m "Your commit message here"  :: Commit with a message
-git push origin main     :: Push to the main branch
-
-echo mydata.csv has been pushed successfully.
-pause                    :: Pause to keep the window open and see results
+echo ==============================================
+echo Base de datos ha sido respaldada exitosamente.
+echo ==============================================
+pause 
 
