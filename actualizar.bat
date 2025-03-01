30df7f8b177502d5396f6a6f1648dabad1502abd
laRedCr <ventas@laredcr.com>
2025-03-01 09:07:08 -0600

    NEW: se agrega script para actualizar el branch

---
+++ actualizar.bat
@@ -0,0 +1,20 @@
+@echo off
+
+ 
+echo ===========================
+echo    Sistema de respaldo
+echo 	v1.0
+echo	git branch distleimi
+echo ===========================
+
+echo ATENCION: Asegurese de tener conexion a internet...
+Pause
+
+git checkout distleimi
+git pull origin distleimi
+
+echo ==============================================
+echo Base de datos ha sido Actualizada exitosamente.
+echo ==============================================
+pause 
+
