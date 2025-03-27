e90414233f96257b2606d9df8d4ea9f0999eafa2
laRedCr <ventas@laredcr.com>
2025-03-26 18:58:10 -0600

    Base de datos respaldada exitosamente

--- actualizar.bat
+++ actualizar.bat
@@ -4,14 +4,14 @@
 echo ===========================
 echo    Sistema de respaldo
 echo 	v1.0
-echo	git branch distleimi
+echo	git branch Main
 echo ===========================
 
 echo ATENCION: Asegurese de tener conexion a internet...
 Pause
 
-git checkout distleimi
-git pull origin distleimi
+git checkout main
+git pull origin main
 
 echo ==============================================
 echo Base de datos ha sido Actualizada exitosamente.
--- respaldo.bat
+++ respaldo.bat
@@ -4,16 +4,20 @@
 echo ===========================
 echo    Sistema de respaldo
 echo 	v1.0
-echo	git branch distleimi
+echo	git branch Main
 echo ===========================
 
 echo ATENCION: Asegurese de tener conexion a internet...
 Pause
-
-git add inv_backEnd.accdb 
+echo
+git status
+echo
+git add .
+echo ==============================================
 git commit -m "Base de datos respaldada exitosamente"
+echo ==============================================
 git push
-
+echo
 echo ==============================================
 echo Base de datos ha sido respaldada exitosamente.
 echo ==============================================
